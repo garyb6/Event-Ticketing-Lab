05ec9ffb5b88fe5f57e094a7a552ea97d442dc8e
garyb6 <gary.bennett6@outlook.com>
2021-11-25 14:49:09 +0000

    added event intialisation and first method for event name

--- src/event.py
+++ src/event.py
@@ -2,8 +2,9 @@ from tests import performer_test
 
 
 class Event:
-    def __init__(self, name, ticket_price, customer_list, performer_list):
+    def __init__(self, name, ticket, ticket_price, customer_list, performer_list):
         self.name = name
+        self.ticket = ticket
         self.ticket_price = ticket_price 
         self.customer_list = customer_list 
         self.performer_list = performer_list
--- tests/event_test.py
+++ tests/event_test.py
@@ -7,7 +7,10 @@ class TestEvent(unittest.TestCase):
     def setUp(self):
         self.cartman = Customer ("Cartman", 20.00, "Chef")
         self.chef = Performer ("Chef")
-        self.event = Event ("Cochella", 10.00,[self.cartman], [self.chef])
+        self.event = Event ("Cochella", 100, 10.00,[self.cartman], [self.chef])
 
     def test_event_has_name(self):
         self.assertEqual ("Cochella", self.event.name)
+    
+    def test_sell_ticket(self, customer):
+        pass 
